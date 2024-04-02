abf3d0636c36a681275d81bbbddbe19e888ad3a7
Giordano Zannoni <info@artisgrafica.com>
2024-04-02 11:49:00 +0200

    no message

--- single_blocks.php
+++ single_blocks.php
@@ -133,7 +133,12 @@ if (have_posts()) :
                                     <p><?php echo $descrizioneHotel; ?></p>
                                 </div>                   
                                 <div class="cta-container">
-                                    <button style="display:none" class="text-read-more"></button><a href="<?php echo $linkHotel; ?>" title="Visita <?php echo $imgHotel["title"] ?> su Booking.com" target="_blank" class="hotel-cta fasc-button fasc-size-large fasc-type-flat fasc-rounded-medium ico-fa fasc-ico-after fa-chevron-right"><?php echo _e('vedi_info_e_tariffe', 'top10hotel');?></a>
+                                    <button style="display:none" class="text-read-more"></button>
+                                    <a href="<?php echo $linkHotel; ?>" title="Visita <?php echo $imgHotel["title"] ?> su Booking.com" target="_blank" class="hotel-cta"><?php echo _e('vedi_info_e_tariffe', 'top10hotel');?>
+                                        <svg class="svg-icon" viewBox="0 0 20 20">
+                                            <path fill="#ffffff" d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z"></path>
+                                        </svg>
+                                    </a>
                                 </div>
                                 
                             </section>
