dc4fe410e1e2db197d4f06b55fd87d67f222989d
Giordano Zannoni <info@artisgrafica.com>
2024-05-21 11:01:43 +0200

    no message

--- functions.php
+++ functions.php
@@ -39,3 +39,36 @@ function defer_parsing_of_js( $url ) {
     return str_replace( ' src', ' defer src', $url );
 }
 add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
+
+
+// Disabilita il controllo di unicità degli slug per i post
+function allow_duplicate_slugs($slug, $post_ID, $post_status, $post_type, $post_parent, $original_slug) {
+    if (function_exists('pll_default_language')) {
+        return $original_slug;
+    }
+    return $slug;
+}
+add_filter('wp_unique_post_slug', 'allow_duplicate_slugs', 10, 6);
+
+// Disabilita il controllo di unicità degli slug per i termini
+function allow_duplicate_term_slugs($slug, $term, $original_slug, $original_term, $args) {
+    if (function_exists('pll_default_language')) {
+        return $original_slug;
+    }
+    return $slug;
+}
+add_filter('wp_unique_term_slug', 'allow_duplicate_term_slugs', 10, 6);
+
+// Disabilita il redirect canonico di Polylang
+add_filter('pll_check_canonical_url', '__return_false');
+
+// Flush delle regole di riscrittura
+function custom_flush_rewrite_rules() {
+    flush_rewrite_rules();
+}
+add_action('after_switch_theme', 'custom_flush_rewrite_rules');
+
+// Disabilita redirezioni canoniche di WordPress temporaneamente (solo per debug)
+remove_filter('template_redirect', 'redirect_canonical');
+
+
